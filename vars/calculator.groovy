f3a0b044c7338a8283e52934af631d1639b987f9
Nithin Prince John <nithinpj09@gmail.com>
2023-02-27 12:26:45 +0530

    Create calculator.groovy

---
+++ vars/calculator.groovy
@@ -0,0 +1,15 @@
+def add(x, y) {
+  echo "The sum of ${x} and ${y} is ${x+y}"
+}
+
+def sub(x, y) {
+  echo "The sum of ${x} and ${y} is ${x-y}"
+}
+
+def mul(x, y) {
+  echo "The sum of ${x} and ${y} is ${x*y}"
+}
+
+def div(x, y) {
+  echo "The sum of ${x} and ${y} is ${x/y}"
+}
