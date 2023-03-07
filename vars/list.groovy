1303c8b1dd8758130103815b27f1a11187df974f
Nithin Prince John <nithinpj09@gmail.com>
2023-03-07 09:43:50 +0530

    Update list.groovy

--- vars/list.groovy
+++ vars/list.groovy
@@ -1,8 +1,3 @@
 def call() {
-  def y = ["Guru99", "is", "Best", "for", "Groovy"]
-  println y
-  y.add("Learning")
-  println(y.contains("is"))
-  println(y.get(2))
-  println(y.pop())
+  echo "Nithin"
 }
