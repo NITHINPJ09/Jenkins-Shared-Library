0aee2916c0e7e2a78b01c72a6d5cc47b6f370c03
Nithin Prince John <nithinpj09@gmail.com>
2023-03-07 09:52:27 +0530

    Update and rename list.groovy to test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -2,7 +2,7 @@ def call() {
   def y = ["Guru99", "is", "Best", "for", "Groovy"]
   println y
   y.add("Learning")
-  println(y.contains("is"))
-  println(y.get(2))
-  println(y.pop())
+  println y.contains("is")
+  println y.get(2)
+  println y.pop()
 }
