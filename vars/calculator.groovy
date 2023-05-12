2a0b29765a9af0d82cbfb62d7423da17f8a8e803
Nithin Prince John <nithinpj09@gmail.com>
2023-05-12 16:00:10 +0530

    Update calculator.groovy

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -3,13 +3,13 @@ def add(x, y) {
 }
 
 def sub(x, y) {
-  echo "The sum of ${x} and ${y} is ${x-y}"
+  echo "The sub of ${x} and ${y} is ${x-y}"
 }
 
 def mul(x, y) {
-  echo "The sum of ${x} and ${y} is ${x*y}"
+  echo "The mul of ${x} and ${y} is ${x*y}"
 }
 
 def div(x, y) {
-  echo "The sum of ${x} and ${y} is ${x/y}"
+  echo "The div of ${x} and ${y} is ${x/y}"
 }
